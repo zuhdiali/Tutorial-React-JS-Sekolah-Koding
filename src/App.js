29eb7f9e0d92c1553c5178e9905d7193aa417bb6
Zuhdi24 <222011642@stis.ac.id>
2023-02-01 09:32:04 +0700

    murni tutorial video 4

--- src/App.js
+++ src/App.js
@@ -2,20 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
 
-// Komponen - Properties
-function Biodata(props) {
-  return <span> umurnya {props.age}</span>;
-}
-
-function Greeting(props) {
-  return (
-    <h1>
-      {" "}
-      Halo {props.name} - <Biodata age={props.age} />
-    </h1>
-  );
-}
-
 class Timer extends Component {
   constructor(props) {
     super(props);
@@ -51,22 +37,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Timer start="0" />
-
-        {/* <Greeting name = "Zuhdi Ali Hisyam" age="21"/>
-        <Greeting name = "Zuhdi Ali " age ="20"/>
-        <Greeting name = "Zuhdi "/>  */}
-        {/* <p>
-          Zuhdi 
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
